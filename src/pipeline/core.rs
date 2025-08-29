f135fa4ff0a146ed330ff8ee6ae2d7569b4623be
LugolBis <ld.desmares@gmail.com>
2025-08-29 19:05:13 +0200

    Update pipeline

--- src/pipeline/core.rs
+++ src/pipeline/core.rs
@@ -1,7 +1,6 @@
 use crate::pipeline::task::{task1, task2, task3};
 
 pub async fn main() {
-    /* 
     match task1().await {
         Ok(message) => println!("{}\n\nSuccessfully finished the Task1 !\n\n", message),
         Err(message) => {
@@ -14,7 +13,7 @@ pub async fn main() {
         Err(message) => {
             eprintln!("{}\n\nFailed to run the Task2.\n\n", message);
         }
-    }*/
+    }
     
     match task3() {
         Ok(message) => println!("{}\n\nSuccessfully finished the Task3 !\n\n", message),
