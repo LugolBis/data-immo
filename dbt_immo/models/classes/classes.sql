1417b378e49b8d498cbd3a24ab24df5643188bfb
LugolBis <ld.desmares@gmail.com>
2025-10-20 18:50:11 +0200

    Improved dbt model

--- dbt_immo/models/classes/classes.sql
+++ dbt_immo/models/classes/classes.sql
@@ -1,1 +1,13 @@
-SELECT * FROM DVF."classes.parquet" 
+WITH filled_values AS (
+    SELECT
+        m.idg as idg,
+        COALESCE(c.libelle, 'Unknown') AS libelle,
+        COALESCE(c.surface, 1.0) AS surface
+    FROM
+        {{ ref("mutations") }} AS m
+        LEFT OUTER JOIN DVF."classes.parquet" AS c
+        ON m.idg = c.idg
+)
+
+SELECT *
+FROM filled_values
